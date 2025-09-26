4e58805d14e65bb499a767c7786424b3b9df853a
luisnegrete <jose.negrete@globant.com>
2025-09-26 12:16:14 +0200

    Reports fixed

--- src/test/java/com/saucedemo/runners/RunCucumberTest.java
+++ src/test/java/com/saucedemo/runners/RunCucumberTest.java
@@ -1,27 +1,16 @@
 package com.saucedemo.runners;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "src/test/resources/features",
-    glue = {"com.saucedemo.steps", "com.saucedemo.hooks"},
-    plugin = {
+@CucumberOptions(features = "${cucumber.features}", glue = { "com.saucedemo.steps", "com.saucedemo.hooks" }, plugin = {
         "pretty",
+        "rerun:target/rerun.txt",
         "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
         "timeline:target/cucumber-parallel-report"
-    },
-    monochrome = true
-)
+}, monochrome = true)
 
 public class RunCucumberTest {
-    static {
-        String timestamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
-        System.setProperty("allure.results.directory", "target/allure-results-" + timestamp);
-    }
 }
