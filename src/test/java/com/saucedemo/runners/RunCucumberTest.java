1417cfa98570a39e83af2dd88eefe01fd434007b
luisnegrete <jose.negrete@globant.com>
2025-09-29 12:42:55 +0200

    Rerun failed scenarios

--- src/test/java/com/saucedemo/runners/RunCucumberTest.java
+++ src/test/java/com/saucedemo/runners/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "${cucumber.features}", glue = { "com.saucedemo.steps", "com.saucedemo.hooks" }, plugin = {
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.saucedemo.steps", "com.saucedemo.hooks" }, plugin = {
         "pretty",
         "rerun:target/rerun.txt",
         "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
---
+++ src/test/java/com/saucedemo/runners/RunFailedTests.java
@@ -0,0 +1,19 @@
+package com.saucedemo.runners;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "@target/rerun.txt",
+    glue = {"com.saucedemo.steps", "com.saucedemo.hooks"},
+    plugin = {
+        "pretty",
+        "html:target/rerun-reports/cucumber.html",
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+    },
+    monochrome = true
+)
+public class RunFailedTests {
+}
